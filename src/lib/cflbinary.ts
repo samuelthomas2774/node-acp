4a8958ef4d3e361c1aa40c7199a77802e8deda4d
Samuel Elliott <samuel@fancy.org.uk>
2019-11-16 20:14:02 +0000

    Use buffers and support bigints in CFLBinaryPLists

--- src/lib/cflbinary.ts
+++ src/lib/cflbinary.ts
@@ -34,13 +34,11 @@ export class CFLBinaryPListComposer {
      * @return {string} data
      */
     static compose(object: any) {
-        let data = HEADER_MAGIC;
-
-        data += this.packObject(object);
-
-        data += FOOTER_MAGIC;
-
-        return Buffer.from(data, 'binary');
+        return Buffer.concat([
+            Buffer.from(HEADER_MAGIC, 'binary'),
+            this.packObject(object),
+            Buffer.from(FOOTER_MAGIC, 'binary'),
+        ]);
     }
 
     /**
@@ -50,13 +48,11 @@ export class CFLBinaryPListComposer {
      * @param {number} depth
      * @return {string} data
      */
-    static packObject(object: any, depth = 1) {
-        let data = '';
-
+    static packObject(object: any, depth = 1): Buffer {
         if (object === undefined || object === null) {
-            data += '\x00';
+            return Buffer.from([0x00]);
         } else if (typeof object === 'boolean') {
-            data += object ? '\x09' : '\x08';
+            return Buffer.from([object ? 0x09 : 0x08]);
         } else if (typeof object === 'number' && object % 1 !== 0) {
             let string = '';
             let size: string | number | null = null;
@@ -81,9 +77,7 @@ export class CFLBinaryPListComposer {
             }
 
             const marker = 0x20 + Math.log2(string.length);
-
-            data += String.fromCharCode(marker);
-            data += string;
+            return Buffer.concat([Buffer.from([marker]), Buffer.from(string, 'binary')]);
         } else if (typeof object === 'number') {
             let string = '';
             let size = null;
@@ -108,55 +102,70 @@ export class CFLBinaryPListComposer {
             }
 
             const marker = 0x10 + Math.log2(string.length);
+            return Buffer.concat([Buffer.from([marker]), Buffer.from(string, 'binary')]);
+        } else if (typeof object === 'bigint') {
+            const buffer = Buffer.alloc(8);
+            buffer.writeBigUInt64BE(object, 0);
 
-            data += String.fromCharCode(marker);
-            data += string;
+            const marker = 0x10 + Math.log2(8);
+            return Buffer.concat([Buffer.from([marker]), buffer]);
         } else if (typeof object === 'object' && object instanceof Buffer) {
-            if (object.length >= 0xf) {
-                data += String.fromCharCode(0x4f);
-                data += this.packObject(object.length, depth + 1);
-            } else {
-                data += String.fromCharCode(0x40 + object.length);
-            }
-
-            data += object.toString('binary');
+            return Buffer.concat([
+                object.length >= 0xf ? Buffer.concat([
+                    Buffer.from([0x4f]),
+                    this.packObject(object.length, depth + 1),
+                ]) : Buffer.from([0x40 + object.length]),
+                object,
+            ]);
         } else if (typeof object === 'string') {
-            data += '\x70';
-            data += Buffer.from(object, 'utf8').toString('binary');
-            data += '\x00';
+            return Buffer.concat([
+                Buffer.from([0x70]),
+                Buffer.from(object, 'utf-8'),
+                Buffer.from([0x00]),
+            ]);
         } else if (typeof object === 'object' && object instanceof Array || object instanceof Set) {
-            data += '\xa0';
+            const objects = [];
 
             for (let element of object as any[]) {
-                data += this.packObject(element, depth + 1);
+                objects.push(this.packObject(element, depth + 1));
             }
 
-            data += '\x00';
+            return Buffer.concat([
+                Buffer.from([0xa0]),
+                ...objects,
+                Buffer.from([0x00]),
+            ]);
         } else if (typeof object === 'object' && object instanceof Map) {
-            data += '\xd0';
+            const objects = [];
 
             for (let [key, value] of object.entries() as unknown as [any, any][]) {
-                data += this.packObject(key, depth + 1);
-                data += this.packObject(value, depth + 1);
+                objects.push(this.packObject(key, depth + 1));
+                objects.push(this.packObject(value, depth + 1));
             }
 
-            data += '\x00';
+            return Buffer.concat([
+                Buffer.from([0xd0]),
+                ...objects,
+                Buffer.from([0x00]),
+            ]);
         } else if (typeof object === 'object') {
-            data += '\xd0';
+            const objects = [];
 
             for (let key in object) {
                 if (!object.hasOwnProperty(key)) continue;
 
-                data += this.packObject(key, depth + 1);
-                data += this.packObject(object[key], depth + 1);
+                objects.push(this.packObject(key, depth + 1));
+                objects.push(this.packObject(object[key], depth + 1));
             }
 
-            data += '\x00';
+            return Buffer.concat([
+                Buffer.from([0xd0]),
+                ...objects,
+                Buffer.from([0x00]),
+            ]);
         } else {
             throw new Error('Unsupported object');
         }
-
-        return data;
     }
 }
 
@@ -168,24 +177,24 @@ export class CFLBinaryPListParser {
      * @return {*}
      */
     static parse(data: Buffer | string) {
-        if (data instanceof Buffer) data = data.toString('binary');
+        if (typeof data === 'string') data = Buffer.from(data, 'binary');
 
         if (data.length < HEADER_SIZE + FOOTER_SIZE + 1) {
             throw new Error('Not enough data to parse');
         }
 
-        const header_data = data.substr(0, HEADER_SIZE);
+        const header_data = data.slice(0, HEADER_SIZE).toString('binary');
         if (header_data !== HEADER_MAGIC) {
             throw new Error('Bad header magic');
         }
 
-        const [object, remaining_data] = this.unpackObject(data.substr(HEADER_SIZE));
+        const [object, remaining_data] = this.unpackObject(data.slice(HEADER_SIZE));
 
         if (remaining_data.length > FOOTER_SIZE) {
-            throw new Error('Extra data found after unpacking root object: expected ' + FOOTER_SIZE + ' but found ' + remaining_data.length + ' - ' + Buffer.from(remaining_data, 'binary').toString('hex'));
+            throw new Error('Extra data found after unpacking root object: expected ' + FOOTER_SIZE + ' but found ' + remaining_data.length + ' - ' + remaining_data.toString('hex'));
         }
 
-        if (remaining_data !== FOOTER_MAGIC) {
+        if (remaining_data.toString('binary') !== FOOTER_MAGIC) {
             throw new Error('Bad footer magic');
         }
 
@@ -195,11 +204,11 @@ export class CFLBinaryPListParser {
     /**
      * Unpack an object from the provided data.
      *
-     * @param {string} data
+     * @param {Buffer} data
      * @param {number} depth
      * @return {Array}
      */
-    static unpackObject(data: string, depth = 1): [any, string] {
+    static unpackObject(data: Buffer, depth = 1): [any, Buffer] {
         if (depth > 10) {
             throw new Error('Max depth reached');
         }
@@ -237,7 +246,7 @@ export class CFLBinaryPListParser {
             let size;
             [size, data] = this.unpackCount(object_info, data);
 
-            return [Buffer.from(data.substr(0, size), 'binary'), data.substr(size)];
+            return [data.slice(0, size), data.slice(size)];
         } else if (object_type === 0x50) {
             // ASCII string
             throw new Error('ASCII strings not implemented');
@@ -246,23 +255,8 @@ export class CFLBinaryPListParser {
             throw new Error('Unicode strings not implemented');
         } else if (object_type === 0x70) {
             // null terminated UTF8 string
-            let string = '';
-
-            while (true) {
-                if (!data.length) {
-                    throw new Error('Not enough data');
-                }
-
-                const byte = data.substr(0, 1);
-                data = data.substr(1);
-
-                if (byte === '\x00') break;
-
-                string += byte;
-            }
-
-            const object = Buffer.from(string, 'binary').toString('utf8');
-            return [object, data];
+            const size = data.indexOf(0x00);
+            return [data.slice(0, size).toString('utf-8'), data.slice(size + 1)];
         } else if (object_type === 0x80) {
             // uid
             throw new Error('uids not implemented');
@@ -312,48 +306,50 @@ export class CFLBinaryPListParser {
     /**
      * Unpack an object marker from the provided data.
      *
-     * @param {string} data
+     * @param {Buffer} data
      * @return {Array}
      */
-    static unpackObjectMarker(data: string): [number, string] {
-        const marker_byte = data.substr(0, 1);
-
-        const marker = Buffer.from(marker_byte, 'binary').readInt8(0);
-
-        return [marker, data.substr(1)];
+    static unpackObjectMarker(data: Buffer): [number, Buffer] {
+        return [
+            data.readInt8(0),
+            data.slice(1),
+        ];
     }
 
     /**
      * Unpack an int object as a JavaScript number from the provided data.
      *
      * @param {number} size_exponent
-     * @param {string} data
+     * @param {Buffer} data
      * @return {Array}
      */
-    static unpackInt(size_exponent: number, data: string): [number, string] {
+    static unpackInt(size_exponent: number, data: Buffer): [number | bigint, Buffer] {
         const int_size = 2 ** size_exponent;
-        const int_bytes = data.substr(0, int_size);
-        data = data.substr(int_size);
 
-        return [Buffer.from(int_bytes, 'binary').readUIntBE(0, int_size), data];
+        return [
+            int_size === 8 ?
+                data.readBigUInt64BE(0) :
+                data.readUIntBE(0, int_size),
+            data.slice(int_size),
+        ];
     }
 
     /**
      * Unpack a real object as a JavaScript number from the provided data.
      *
      * @param {number} size_exponent
-     * @param {string} data
+     * @param {Buffer} data
      * @return {Array}
      */
-    static unpackReal(size_exponent: number, data: string): [number, string] {
+    static unpackReal(size_exponent: number, data: Buffer): [number, Buffer] {
         const real_size = 2 ** size_exponent;
-        const real_bytes = data.substr(0, real_size);
-        data = data.substr(real_size);
+        const real_bytes = data.slice(0, real_size);
+        data = data.slice(real_size);
 
         if (real_size === 4) {
-            return [Buffer.from(real_bytes, 'binary').readFloatBE(0), data];
+            return [real_bytes.readFloatBE(0), data];
         } else if (real_size === 8) {
-            return [Buffer.from(real_bytes, 'binary').readDoubleBE(0), data];
+            return [real_bytes.readDoubleBE(0), data];
         }
 
         throw new Error('Unsupported real packed object size of ' + real_size + ' bytes');
@@ -363,10 +359,10 @@ export class CFLBinaryPListParser {
      * Unpack count from object info nibble and/or packed int value.
      *
      * @param {number} object_info
-     * @param {string} data
+     * @param {Buffer} data
      * @return {Array}
      */
-    static unpackCount(object_info: number, data: string): [number, string] {
+    static unpackCount(object_info: number, data: Buffer): [number, Buffer] {
         if (object_info === 0x0f) {
             // Count is the following packed int object
 
@@ -380,7 +376,11 @@ export class CFLBinaryPListParser {
                 throw new Error('Expected count to be a packed int object');
             }
 
-            return this.unpackInt(count_object_info, data);
+            let count;
+            [count, data] = this.unpackInt(count_object_info, data);
+
+            if (typeof count === 'bigint') throw new Error('count is a BigInt??');
+            return [count, data];
         }
 
         return [object_info, data];
